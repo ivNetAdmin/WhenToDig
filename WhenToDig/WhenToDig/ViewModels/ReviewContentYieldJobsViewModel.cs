7bf61083be45fb8788ebfd0d2dbbe683e6c3c5cd
ivNetAdmin <bp@ivnet.co.uk>
2018-01-23 16:56:32 +0000

    Review Jobs

--- WhenToDig/WhenToDig/ViewModels/ReviewContentYieldJobsViewModel.cs
+++ WhenToDig/WhenToDig/ViewModels/ReviewContentYieldJobsViewModel.cs
@@ -1,14 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using WhenToDig.Models;
 using Xamarin.Forms;
 
 namespace WhenToDig.ViewModels
 {
     public class ReviewContentYieldJobsViewModel : BaseModel
     {
+        private Yield _yield;
         public ReviewContentYieldJobsViewModel(INavigation navigation, string yieldId)
         {
+            this.Navigation = navigation;
+            Title = "Yield Jobs";
+
+            _yield = _realmInstance.Find<Yield>(yieldId);
+
+            var relatedJobs = GetRelatedJobs(_yield.Plant, _yield.Year);
+            var unrelatedJobs = GetUnRelatedJobs(_yield.Year);
+        }
+
+        private List<Job> GetRelatedJobs(string plant,int year)
+        {
+            var startDate = new DateTimeOffset(new DateTime(year - 1, 9, 30));
+            var endDate = new DateTimeOffset(new DateTime(year, 10, 01));
+            return new List<Job>(
+                _realmInstance.All<Job>()
+                .Where(x => x.Plant == plant && x.Date > startDate && x.Date < endDate)
+                .OrderBy(x => x.Type)
+                .ThenBy(x => x.Date).ToList());
+
+        }
+        private List<Job> GetUnRelatedJobs(int year)
+        {
+            var startDate = new DateTimeOffset(new DateTime(year - 1, 9, 30));
+            var endDate = new DateTimeOffset(new DateTime(year, 10, 01));
+            return new List<Job>(
+                _realmInstance.All<Job>()
+                .Where(x => x.Type == "General" && x.Date > startDate && x.Date < endDate)
+                .OrderBy(x => x.Type)
+                .ThenBy(x => x.Date).ToList());
 
         }
     }
