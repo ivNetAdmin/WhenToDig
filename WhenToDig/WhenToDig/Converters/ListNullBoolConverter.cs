8c900ae20256cf641ddcb64e137e8ce8846b5bb7
ivNetAdmin <bp@ivnet.co.uk>
2018-01-24 10:21:58 +0000

    Review Yield Job

---
+++ WhenToDig/WhenToDig/Converters/ListNullBoolConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WhenToDig.Converters
+{
+    public class ListNullBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            
+            var l = (IEnumerable<object>)value;
+            if (l.Count() == 0) return false;
+            return true;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
